707f5694a19358505f618ea21ba22edcb07a4e78
Timoshka177 <107550627+Timoshka177@users.noreply.github.com>
2025-01-14 13:39:40 +0100

    mise a jour bdd

--- inc/init.php
+++ inc/init.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connexion à la BDD
-$pdo = new PDO('mysql:host=profeszmarc.mysql.db;dbname=profeszshop','profeszshop','Shop2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=profeszshop.mysql.db;dbname=profeszshop','profeszshop','Shop2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 require_once("fonction.php");
 
